39fe02b5daeebd3debd65eae0354265c38343acd
intrnl <intrnl28+git@gmail.com>
2021-12-23 12:12:49 +0700

    use new resolve method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import * as esbuild from 'esbuild';
-import * as path from 'path';
 
 
 /**
@@ -13,16 +12,14 @@ export default function aliasPlugin (options = {}) {
 	return {
 		name: '@intrnl/esbuild-plugin-alias',
 		setup (build) {
-			const resolve = shouldResolve && createResolveContainer(build.initialOptions);
-
 			build.onResolve({ filter: matcher }, async (args) => {
 				const nextId = args.path.replace(matcher, (_, id, end) => {
 					return entries[id] + end;
 				});
 
 				if (shouldResolve) {
-					const resolved = await resolve(nextId, args.importer);
-					return { path: resolved };
+					let result = build.resolve(nextId, args);
+					return result;
 				}
 				else {
 					return { path: nextId };
@@ -42,59 +39,4 @@ function createMatcher (object) {
 
 function escapeRE (string) {
 	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-
-const excluded = new Set([
-	'@intrnl/esbuild-plugin-alias',
-	'@intrnl/esbuild-plugin-build-analysis',
-	'@intrnl/esbuild-plugin-env',
-]);
-
-/**
- * @param {esbuild.PluginOptions} buildOptions
- */
-function createResolveContainer (buildOptions) {
-	const customOptions = {
-		...buildOptions,
-		inject: [],
-		define: {},
-		logLevel: 'silent',
-		write: false,
-		entryPoints: undefined,
-		publicPath: undefined,
-		splitting: false,
-		plugins: buildOptions.plugins?.filter((plugin) => !excluded.has(plugin.name)) || [],
-	};
-
-	return async function resolve (source, importer) {
-		let result;
-
-		/** @type {esbuild.Plugin} */
-		const resolvePlugin = {
-			name: 'resolve',
-			setup (build) {
-				build.__resolve = true;
-
-				build.onLoad({ filter: /.*/ }, (args) => {
-					result = args.path;
-					return { contents: '' };
-				});
-			},
-		};
-
-		await esbuild.build({
-			...customOptions,
-			stdin: {
-				contents: `import ${JSON.stringify(source)};`,
-				resolveDir: path.dirname(importer || source),
-			},
-			plugins: [
-				resolvePlugin,
-				...customOptions.plugins,
-			],
-		});
-
-		return result;
-	}
 }
